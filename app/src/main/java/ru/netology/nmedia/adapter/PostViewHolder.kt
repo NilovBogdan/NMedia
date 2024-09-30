64a2ee4c11deda9b393315a391640141961fb561
Nilov Bogdan <bnilov106@gmil.com>
2024-09-30 21:02:37 +0300

    Intents

--- app/src/main/java/ru/netology/nmedia/adapter/PostViewHolder.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostViewHolder.kt
@@ -1,6 +1,5 @@
 package ru.netology.nmedia.adapter
 
-import android.content.Intent
 import android.view.View
 import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
